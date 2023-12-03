005ea530b4a3722ec459311025b1a7d108fa97ed
katjasdf <katjasdf>
2023-12-03 13:15:02 +0200

    day 1, part 2 done (finally...)

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,18 +1,52 @@
-lines = open("day_1_data.txt", "r").readlines()
-numbers = []
-response = 0
+import re
 
-for line in lines:
-    line_numbers = []
-    letters = list(line)
-    for letter in letters:
-        if letter.isnumeric():
-            line_numbers.append(letter)
+lines = open('day_1_data.txt', 'r').readlines()
 
-    two_numbers = line_numbers[0] + line_numbers[-1]
-    numbers.append(two_numbers)
+def first_part():
+    numbers = []
+    response = 0
 
-for number in numbers:
-    response += int(number)
+    for line in lines:
+        line_numbers = []
+        for letter in line:
+            if letter.isnumeric():
+                line_numbers.append(letter)
 
-print(response)
+        two_numbers = line_numbers[0] + line_numbers[-1]
+        numbers.append(two_numbers)
+
+    for number in numbers:
+        response += int(number)
+
+    print(response)
+
+def second_part():
+    written_numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    numbers = []
+    response = 0
+
+    for line in lines:
+        line_numbers = []
+        for i, letter in enumerate(line, start=1):
+            if letter.isnumeric():
+                line_numbers.append((letter, i))
+        
+        for i, written_number in enumerate(written_numbers, start=1):
+            all_matches = re.finditer(written_number, line)
+            
+            for match in all_matches:
+                matches = str(i), match.start()
+                line_numbers.append(matches)
+        
+        sorted_numbers = sorted(line_numbers, key=lambda x: x[1])
+
+        two_numbers = sorted_numbers[0][0] + sorted_numbers[-1][0]
+        numbers.append(two_numbers)
+    
+    for number in numbers:
+        response += int(number)
+
+    print(response)
+
+first_part()
+second_part()
